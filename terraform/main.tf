6fdcd4e68e1fa28b336facb023fbc1f76f55d452
deboraferrarids2 <deboraferrarids2@gmail.com>
2024-10-01 11:47:01 -0300

    terraform update

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,73 +5,73 @@ provider "aws" {
 # Data source to get available availability zones
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_v2" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
-    Name = "eks-vpc"
+    Name = "eks-vpc-v2"
   }
 }
 
 # Criar subnets públicas e privadas
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_v2" {
   count = 2
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main_v2.id
   cidr_block = "10.0.${count.index}.0/24"
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name = "public-subnet-${count.index}"
+    Name = "public-subnet-v2-${count.index}"
     Tier  = "public"
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_v2" {
   count = 2
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main_v2.id
   cidr_block = "10.0.${count.index + 2}.0/24"
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name = "private-subnet-${count.index}"
+    Name = "private-subnet-v2-${count.index}"
     Tier  = "private"
   }
 }
 
 # Criar um gateway de internet
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "igw_v2" {
+  vpc_id = aws_vpc.main_v2.id
 
   tags = {
-    Name = "eks-igw"
+    Name = "eks-igw-v2"
   }
 }
 
 # Criar uma tabela de rotas para a subnet pública
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "public_v2" {
+  vpc_id = aws_vpc.main_v2.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.igw_v2.id
   }
 
   tags = {
-    Name = "public-route-table"
+    Name = "public-route-table-v2"
   }
 }
 
 # Associar a tabela de rotas às subnets públicas
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public_v2" {
   count = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_v2[count.index].id
+  route_table_id = aws_route_table.public_v2.id
 }
 
 # Criar o IAM role para o EKS
-resource "aws_iam_role" "eks_role" {
-  name = "eks-role"
+resource "aws_iam_role" "eks_role_v2" {
+  name = "eks-role-v2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -80,36 +80,39 @@ resource "aws_iam_role" "eks_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "eks.amazonaws.com"
+          "Service": [
+                    "ec2.amazonaws.com",
+                    "eks.amazonaws.com"
+                ]
         }
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_policy_attach" {
+resource "aws_iam_role_policy_attachment" "eks_policy_attach_v2" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_role.name
+  role       = aws_iam_role.eks_role_v2.name
 }
 
 # Criar o cluster EKS
-resource "aws_eks_cluster" "eks" {
-  name     = "eks-cluster"
-  role_arn = aws_iam_role.eks_role.arn
+resource "aws_eks_cluster" "eks_v2" {
+  name     = "eks-cluster-v2"
+  role_arn = aws_iam_role.eks_role_v2.arn
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.private[0].id,
-      aws_subnet.private[1].id,
+      aws_subnet.private_v2[0].id,
+      aws_subnet.private_v2[1].id,
     ]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.eks_policy_attach]
+  depends_on = [aws_iam_role_policy_attachment.eks_policy_attach_v2]
 }
 
 # Criar o IAM role para o Fargate
-resource "aws_iam_role" "eks_fargate_role" {
-  name = "eks-fargate-role"
+resource "aws_iam_role" "eks_fargate_role_v2" {
+  name = "eks-fargate-role-v2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -125,33 +128,31 @@ resource "aws_iam_role" "eks_fargate_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "fargate_policy_attach" {
+resource "aws_iam_role_policy_attachment" "fargate_policy_attach_v2" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-  role       = aws_iam_role.eks_fargate_role.name
+  role       = aws_iam_role.eks_fargate_role_v2.name
 }
 
-
 # Criar o perfil de Fargate
-resource "aws_eks_fargate_profile" "fargate_profile" {
-  cluster_name           = aws_eks_cluster.eks.name
-  fargate_profile_name   = "my-fargate-profile"
-  pod_execution_role_arn = aws_iam_role.eks_fargate_role.arn
+resource "aws_eks_fargate_profile" "fargate_profile_v2" {
+  cluster_name           = aws_eks_cluster.eks_v2.name
+  fargate_profile_name   = "my-fargate-profile-v2"
+  pod_execution_role_arn = aws_iam_role.eks_fargate_role_v2.arn
   subnet_ids             = [
-    aws_subnet.private[0].id,
-    aws_subnet.private[1].id,
+    aws_subnet.private_v2[0].id,
+    aws_subnet.private_v2[1].id,
   ]
 
   selector {
     namespace = "default"
   }
 
-  depends_on = [aws_eks_cluster.eks]
+  depends_on = [aws_eks_cluster.eks_v2]
 }
 
-
 # Criar um grupo de segurança para o RDS
-resource "aws_security_group" "db_sg" {
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "db_sg_v2" {
+  vpc_id = aws_vpc.main_v2.id
 
   ingress {
     from_port   = 5432
@@ -168,25 +169,25 @@ resource "aws_security_group" "db_sg" {
   }
 
   tags = {
-    Name = "db-sg"
+    Name = "db-sg-v2"
   }
 }
 
 # Criar o DB Subnet Group
-resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "my-db-subnet-group"
+resource "aws_db_subnet_group" "db_subnet_group_v2" {
+  name       = "my-db-subnet-group-v2"
   subnet_ids = [
-    aws_subnet.private[0].id,
-    aws_subnet.private[1].id,
+    aws_subnet.private_v2[0].id,
+    aws_subnet.private_v2[1].id,
   ]
   tags = {
-    Name = "my-db-subnet-group"
+    Name = "my-db-subnet-group-v2"
   }
 }
 
 # Criar o banco de dados RDS
-resource "aws_db_instance" "db" {
-  identifier              = "dbinstance"
+resource "aws_db_instance" "db_v2" {
+  identifier              = "dbinstance-v2"
   engine                 = "postgres"
   engine_version         = "15"
   instance_class         = "db.t3.micro"
@@ -195,11 +196,108 @@ resource "aws_db_instance" "db" {
   username               = var.db_username
   password               = var.db_password
   db_name                = var.db_name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name  # Vincular o subnet group
+  vpc_security_group_ids = [aws_security_group.db_sg_v2.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group_v2.name  # Vincular o subnet group
   skip_final_snapshot    = true
+  deletion_protection = true
 
   tags = {
-    Name = "dbinstance"
+    Name = "dbinstance-v2"
   }
+}
+
+data "aws_ami" "eks_worker" {
+  most_recent = true
+  owners      = ["602401143452"] # Conta oficial da AWS para EKS
+
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-*-v*"] # Padrão para AMIs de worker EKS
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"] # Verifique a arquitetura correta
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+resource "aws_launch_template" "node_group_template" {
+  name_prefix   = "eks-node-template"
+  image_id      = data.aws_ami.eks_worker.id  # Substitua pela AMI correta
+  instance_type = "t3.micro"
+
+  # Adicionando o Security Group do Cluster EKS
+  vpc_security_group_ids = ["sg-077403eb57b605fd5"]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "eks-node"
+    }
+  }
+}
+
+resource "aws_eks_node_group" "node_group_v2" {
+  cluster_name    = aws_eks_cluster.eks_v2.name
+  node_group_name = "my-node-group-v2"
+  node_role_arn   = aws_iam_role.eks_role_v2.arn
+  subnet_ids      = [
+    aws_subnet.private_v2[0].id,
+    aws_subnet.private_v2[1].id,
+  ]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  # Usando o Launch Template que contém o Security Group
+  launch_template {
+    id      = aws_launch_template.node_group_template.id
+    version = "$Latest"  # ou especifique a versão, se preferir
+  }
+
+  tags = {
+    Name = "my-node-group-v2"
+  }
+
+  depends_on = [aws_eks_cluster.eks_v2]
+}
+
+resource "aws_eks_addon" "addons" {
+  for_each          = { for addon in var.addons : addon.name => addon }
+  cluster_name      = aws_eks_cluster.eks_v2.name  # Atualize para o nome correto do cluster
+  addon_name        = each.value.name
+  addon_version     = each.value.version
+  resolve_conflicts_on_create = "OVERWRITE"  # Durante a criação
+  resolve_conflicts_on_update = "OVERWRITE"   # Durante a atualização
+}
+
+
+variable "addons" {
+  type = list(object({
+    name    = string
+    version = string
+  }))
+  default = [
+    {
+      name    = "kube-proxy"
+      version = "v1.30.0-eksbuild.3"
+    },
+    {
+      name    = "vpc-cni"
+      version = "v1.18.1-eksbuild.3"
+    },
+    {
+      name    = "coredns"
+      version = "v1.11.3-eksbuild.1"
+    }
+  ]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,29 @@
-output "eks_cluster_name" {
-  value = aws_eks_cluster.eks.name
-}
-
+# Output para o endpoint do RDS
 output "rds_endpoint" {
-  value = aws_db_instance.db.endpoint
+  value = aws_db_instance.db_v2.endpoint
 }
 
+# Output para o ID do VPC
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.main_v2.id
+}
+
+# Outputs para subnets privadas
+output "private_subnet_ids" {
+  value = aws_subnet.private_v2[*].id
+}
+
+# Outputs para subnets públicas
+output "public_subnet_ids" {
+  value = aws_subnet.public_v2[*].id
+}
+
+# Output para o endpoint da instância do banco de dados
+output "db_instance_endpoint" {
+  value = aws_db_instance.db_v2.endpoint
+}
+
+# Output para o nome do cluster EKS
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_v2.name
 }
